8625a6b63b94c192aa3b26046811b0169b97954f
VolodymyrTrush <vmtrush@gmail.com>
2022-07-09 16:23:48 +0300

    HW2

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,21 @@ import FeedbackOptions from './components/FeedbackOptions';
 import Statistics from './components/Statistics';
 
 class App extends Component {
-  // Стейт
+  
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
   };
 
-  // Метод, который берёт названия кнопок и по ним прибавляет 1 к стейту
+  
   onLeaveFeedback = event => {
     const label = event.target.textContent;
 
     this.setState(prevState => ({ [label]: (prevState[label] += 1) }));
   };
 
-  // Метод, который суммирует значения отзывов в стейте
+  
   countTotalFeedback = () => {
     const total = Object.keys(this.state).reduce(
       (acc, value) => acc + this.state[value],
@@ -28,7 +28,7 @@ class App extends Component {
     return total;
   };
 
-  // Считает процент хороших отзывов (хорошие умножаем на 100 и делим на общее количество)
+  
   countPositiveFeedbackPercentage = () => {
     const percent = Math.round(
       (this.state.good * 100) / this.countTotalFeedback(),
@@ -44,8 +44,8 @@ class App extends Component {
       <>
         <Section title="Please leave feedback">
           <FeedbackOptions
-            options={this.state} // Передаем в компонент кнопок весь стейт
-            onLeaveFeedback={this.onLeaveFeedback} // Метод для названий кнопок и инкримента
+            options={this.state} 
+            onLeaveFeedback={this.onLeaveFeedback} 
           />
         </Section>
         <Section title="Statistics">
@@ -53,8 +53,8 @@ class App extends Component {
             good={good}
             neutral={neutral}
             bad={bad}
-            total={this.countTotalFeedback} // Передаем в компонент статистики подсчет всех отзывов
-            positivePercentage={this.countPositiveFeedbackPercentage} // Туда же передаем подсчет хороших отзывов
+            total={this.countTotalFeedback} 
+            positivePercentage={this.countPositiveFeedbackPercentage} 
           />
         </Section>
       </>
